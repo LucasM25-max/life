535e3f625220484ac08a18e6111d06ef0b3f32f0
LucasM25-max <LucasM_25@outlook.com>
2026-08-13 20:20:57 +0100

    style: load roadmap UI extension stylesheet

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import './life-v2.css';
 import './life-v3.css';
+import './life-v4.css';
 import LifeAppStable from '../components/LifeAppStable';
 export default function Home(){return <LifeAppStable/>}
