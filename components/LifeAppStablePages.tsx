b2a6e72281f8c185b70a46b85d061ef8da3bb32f
LucasM25-max <LucasM_25@outlook.com>
2026-08-13 20:20:20 +0100

    refactor: aggregate stable Life page modules

---
+++ components/LifeAppStablePages.tsx
@@ -0,0 +1,5 @@
+export {Dashboard,LifeList,Observations,Trips,Locations} from './LifeAppStablePagesA';
+export {Venues,Collections} from './LifeAppStablePagesB';
+export {Catalogue,Taxonomy,Discover,TaxonDetail} from './LifeAppStablePagesC';
+export {MapPage,Countries,Statistics,Milestones,ImportPage,Settings} from './LifeAppStablePagesD';
+export {AppPage} from './LifeAppStablePagesRouter';
